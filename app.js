8799f36817986ccbfc9a38bf1f101538bbbb94e3
vahan.sahakyan <vahan.sahakyan1996@gmail.com>
2022-01-29 02:17:48 +0400

    prevent output if whitespace

--- app.js
+++ app.js
@@ -1,79 +1,69 @@
-const content = document.querySelector(".content");
-const info = document.querySelector(".info");
-const input = document.querySelector(".input");
-const list = document.querySelector(".items-list");
-const names1 = ["q", "qw", "qwe", "qwer", "qwert", "qwerty"];
+const content = document.querySelector('.content');
+const info = document.querySelector('.info');
+const input = document.querySelector('.input');
+const list = document.querySelector('.items-list');
+const names1 = ['q', 'qw', 'qwe', 'qwer', 'qwert', 'qwerty'];
 
 const names = [
-  "Vahan",
-  "Vardan",
-  "Vahe",
-  "Veronika",
-  "Varduhi",
-  "Vilen",
-  "Vlad",
-  "Voskan",
-  "Volodya",
-  "Venera",
-  "Smbat",
-  "Serob",
-  "Sargis",
-  "Suren",
-  "Simon",
-  "Srbuhi",
-  "Siranush",
-  "Spartak",
-  "asdasdasdasdasdasdasdasd",
+  'Vahan',
+  'Vardan',
+  'Vahe',
+  'Veronika',
+  'Varduhi',
+  'Vilen',
+  'Vlad',
+  'Voskan',
+  'Volodya',
+  'Venera',
+  'Smbat',
+  'Serob',
+  'Sargis',
+  'Suren',
+  'Simon',
+  'Srbuhi',
+  'Siranush',
+  'Spartak',
+  'asdasdasdasd',
 ];
 
-info.innerHTML = "<h3><span>TEST</span><strong>DATA</strong></h3>";
-for (const item of names) {
-  info.innerHTML += `<li>${item}</li>`;
-}
+info.innerHTML = '<h3><span>TEST</span><strong>DATA</strong></h3>';
+names.forEach(name => (info.innerHTML += `<li>${name}</li>`));
 
 const isMatching = function (name, input) {
-  let normalizedInput = input
-    .toLowerCase()
-    .replaceAll(".", "")
-    .replaceAll(",", "")
-    .replaceAll(" ", "");
-
+  const normalizedInput = input.toLowerCase().trim().replaceAll('.', '');
+  const charsExceptSpace = new RegExp(/\S/);
   return (
+    charsExceptSpace.test(normalizedInput) &&
     name.toLowerCase().indexOf(normalizedInput) !== -1 &&
-    name.toLowerCase().startsWith(normalizedInput.slice("0"))
+    name.toLowerCase().startsWith(normalizedInput.slice('0'))
   );
 };
 
 let results = [];
 let timeoutId;
-input.addEventListener("input", function () {
-  if (timeoutId) {
-    clearTimeout(timeoutId);
-  }
+input.addEventListener('input', function () {
+  timeoutId && clearTimeout(timeoutId);
+
   if (!input.value) {
-    list.innerHTML = "";
+    list.innerHTML = '';
     clearTimeout(timeoutId);
   }
   timeoutId = setTimeout(function () {
     // resets previous output
-    list.innerHTML = "";
+    list.innerHTML = '';
 
     // check if matching
-    for (const name of names) {
-      if (isMatching(name, input.value)) results.push(name);
-    }
+    names.forEach(name => isMatching(name, input.value) && results.push(name));
 
     // outputs each result to DOM
-    for (const result of results) {
-      list.innerHTML += `<li>${result}</li>`;
-    }
+    results.forEach(result => (list.innerHTML += `<li>${result}</li>`));
 
     // if no input resets output
     if (!input.value) {
-      list.innerHTML = "";
+      list.innerHTML = '';
       clearTimeout(timeoutId);
     }
 
     results = [];
-  }, 500);
+  }, 300);
 });
